82fdb038249b55600907d17c8a232e509c788c4a
ethonwilles <ethonwilles@gmail.com>
2020-04-06 11:37:04 -0600

    asdf

--- app.py
+++ app.py
@@ -41,35 +41,48 @@ def chromedriver(url):
                             print(r.json())
                 privacy_checker = True
             
-        for item in scrape.find_all("a"):
-            words = item.get_text().split()
-            for word in words:
-                if word.lower() == "cookie":
-                    r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie", "url" : url, "html": f'{item.find_parent("div")}', "has_cook" : True})
-                    print(r.json())
-                    checker = True
+        # for item in scrape.find_all("a"):
+        #     words = item.get_text().split()
+        #     for word in words:
+        #         if word.lower() == "cookie":
+        #             r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie", "url" : url, "html": f'{item.find_parent("div")}', "has_cook" : True})
+        #             print(r.json()) r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie", "url" : url, "html": f"{item}", "has_cook" : True})
+        #             checker = True
         for item in scrape.find_all("div"):
-                    for word in item.get_attribute_list('id'):
-                        try:
-                            list_of_words = word.split("-")
-                            for word_item in list_of_words:
-                                if word_item.lower() == "cookie":
-                                    r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie", "url" : url, "html": f"{item}", "has_cook" : True})
-                                    print(r.json())
-                                    checker = True
-
-                        except:
-                            1+1
+            
+            for word in item.get_attribute_list('id'):
+                try:
+                    list_of_words = word.split("-")
+                    for word_item in list_of_words:
+                        if word_item.lower() == "cookie" or word_item.lower() == "cookiebanner" or word_item.lower() == "consent":
+                            r = requests.post("http://localhost:5000/change-scraped-item", json={"url" : url, "html": f"{item}"})
+                            print(r.json())
+                            
+                            checker = True
+                            local_check = True
+                except:
+                    1+1
+            for word in item.get_attribute_list("class"):
+                
+                try:
+                    list_of_words = word.split("-")
+                    for w in list_of_words:
+                        if word == "cc" or word == "cookie":
+                            r = requests.post("http://localhost:5000/change-scraped-item", json={"html" : f"{item}", "url" : url})
+                            print(r.json())
+                            checker = True
+                except:
+                    1+1
         if checker:
             print("Cookie Found!")
         else:
             r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie boolean", "url" : url, "has_cook" : False})
-            print("status code from db ",r.status_code)
+            print(r.text)
         if privacy_checker:
             print("privacy Found!")
         else:
             r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : 'privacy boolean', "url" : url, "has_priv" : False})
-            print("status code from db ",r.status_code)
+            print(r.text)
 for url in urls: 
     privacy_checker = False
     checker = False
@@ -88,24 +101,29 @@ for url in urls:
                             r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice": "privacy","has_priv" : True , "priv_url" : f"https://{url}{item.get('href')}" , "url" : url})
                             print(r.json())
                         privacy_checker = True
-                for item in scrape.find_all("a"):
-                    words = item.get_text().split()
-                    for word in words:
-                        if word.lower() == "cookie":
-                            r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie", "url" : url, "html": f"{item.find_parent('div')}", "has_cook" : True})
-                            print(r.json())
-                            
-                            checker = True
                 for item in scrape.find_all("div"):
+            
                     for word in item.get_attribute_list('id'):
                         try:
                             list_of_words = word.split("-")
                             for word_item in list_of_words:
-                                if word_item.lower() == "cookie":
-                                    r = requests.post("https://admin-dev.rivn.com/audit-results-post", json={"choice" : "cookie", "url" : url, "html": f"{item}", "has_cook" : True})
+                                if word_item.lower() == "cookie" or word_item.lower() == "cookiebanner" or word_item.lower() == "consent":
+                                    r = requests.post("http://localhost:5000/change-scraped-item", json={"url" : url, "html": f"{item}"})
+                                    print(r.json())
+                                    
+                                    checker = True
+                                    local_check = True
+                        except:
+                            1+1
+                    for word in item.get_attribute_list("class"):
+                        
+                        try:
+                            list_of_words = word.split("-")
+                            for w in list_of_words:
+                                if word == "cc" or word == "cookie":
+                                    r = requests.post("http://localhost:5000/change-scraped-item", json={"html" : f"{item}", "url" : url})
                                     print(r.json())
                                     checker = True
-
                         except:
                             1+1
                 if checker:
