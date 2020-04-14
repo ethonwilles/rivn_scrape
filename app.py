8a7189c8bf034115432977f0833d129296e9bc66
ethonwilles <ethonwilles@gmail.com>
2020-04-13 18:34:34 -0600

    asdf

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from time import sleep
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu') 
-
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 privacy_urls = []
 cookies = []
